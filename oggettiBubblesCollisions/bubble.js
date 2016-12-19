cf8c5eec5650b22e93855076da5777b9c78b4b8b
fomi <fomigiorgix@gmail.com>
2016-12-20 00:00:26 +0100

    Add file

--- oggettiBubblesCollisions/bubble.js
+++ oggettiBubblesCollisions/bubble.js
@@ -1,13 +1,10 @@
-
-
 function Bubble(x, y) {
     this.x = x;
     this.y = y;
-    this.radius = 5;
+    this.radius = 10;
     this.col = color(0, 127, 255, 155);
 
     this.display = function() {
-        // stroke(255, 255, 255);
         strokeWeight(0);
         fill(this.col);
         ellipse(this.x, this.y, 2 * this.radius, 2 * this.radius);
@@ -22,12 +19,12 @@ function Bubble(x, y) {
         this.col = color(random(0, 255), random(0, 255), random(0, 255))
     };
 
-    this.intersects = function(other){
-      var d = dist(this.x,this.y,other.x,other.y);
-      if(d < this.radius + other.radius + 4){
-        return true;
-      }else{
-        return false;
-      }
+    this.intersects = function(other) {
+        var d = dist(this.x, this.y, other.x, other.y);
+        if (d < this.radius + other.radius + 4) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- oggettiBubblesCollisions/bubbleCollisions.js
+++ oggettiBubblesCollisions/bubbleCollisions.js
@@ -1,8 +1,7 @@
 var bubbles = [];
 var width = 400;
 var height = 400;
-var n_bubbles = 400;
-var b1, b2;
+var n_bubbles = 30;
 
 function setup() {
     createCanvas(400, 400);
@@ -10,14 +9,9 @@ function setup() {
         bubbles.push(new Bubble(random(0, height), random(0, width)));
     }
 
-    // var reload = select("#reload");
-    // reload.mousePressed(reload_fx);
+    var reload = select("#reload");
+    reload.mousePressed(reload_fx);
 }
-
-// function mousePressed() {
-//     bubbles.push(new Bubble(mouseX, mouseY));
-// };
-
 
 function draw() {
     background(0);
@@ -31,21 +25,11 @@ function draw() {
             }
         }
     }
-
-    // if(b1.intersects(b2)){
-    //   bubbles[1].changeColor();
-    //   b2.changeColor();
-    // }
-    // for (var i = bubbles.length-1; i >= 0; i--) {
-    //     bubbles[i].update();
-    //     bubbles[i].display();
-    //     bubbles[i].restart();
-    // }
 }
 
-// function reload_fx() {
-//     bubbles.splice(0, bubbles.length);
-//     for (var i = 0; i < n_bubbles; i++) {
-//         bubbles.push(new Bubble(random(0, height), random(0, width)));
-//     }
-// }
+function reload_fx() {
+    bubbles.splice(0, bubbles.length);
+    for (var i = 0; i < n_bubbles; i++) {
+        bubbles.push(new Bubble(random(0, height), random(0, width)));
+    }
+}
